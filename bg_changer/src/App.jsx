1c09d731bd907fcc23efc65f6f414d7362b79e82
Saurav Rawat <sauravrawat016@gmail.com>
2024-05-21 22:35:18 +0530

    bg-changer done

---
+++ bg_changer/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color, setColor] = useState("olive")
+
+  return (
+    <div className="w-full h-screen duration-200 " style={{backgroundColor : color}}>
+      
+      <div className="fixed flex flew-wrap justify-center gap-3 bottom-12 inset-x-0 " >
+        <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" onClick={()=>{setColor("blue")}}>
+          Blue
+        </button>
+        <button type="button" className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800" onClick={()=>{setColor("red")}}>
+          Red
+        </button>
+        <button type="button" className="text-white bg-Orange-700 hover:bg-Orange-800 focus:ring-4 focus:ring-Orange-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-orange-600 dark:hover:bg-orange-700 focus:outline-none dark:focus:ring-orange-800" onClick={()=>{setColor("orange")}}>
+          Orange
+        </button>
+        <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800" onClick={()=>{setColor("green")}}>
+          Green
+        </button>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
