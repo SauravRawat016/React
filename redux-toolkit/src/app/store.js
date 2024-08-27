6c549c737280fc8ba73f640f0f0445105a1303bc
Saurav Rawat <sauravrawat016@gmail.com>
2024-08-27 20:24:42 +0530

    todo

---
+++ redux-toolkit/src/app/store.js
@@ -0,0 +1,7 @@
+//configure store
+import {configureStore} from '@reduxjs/toolkit';
+import todoReducer from '../feature/todo/todoSlice' ; 
+
+export const store = configureStore({
+    reducer : todoReducer 
+}) ;
